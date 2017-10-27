8c0dbddbe8ff67c2d92a7c41b426cddd95362151
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:05 +0700

    Remove deprecated st2actions import

--- actions/get_results.py
+++ actions/get_results.py
@@ -3,7 +3,7 @@ import requests
 import urllib
 import urlparse
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 __all__ = [
     'TypeformAction'
